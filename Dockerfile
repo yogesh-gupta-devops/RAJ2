dffa2b6a1b3a1c0a2dd2c19c29f9bcfa12a3fad5
Yogesh Gupta <yogesh.gupta@kochartech.com>
2022-05-31 11:48:32 +0530

    html

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM maven:3.8.5-openjdk-18
-COPY pom.xml /tmp/pom.xml
-RUN mvn -B -f /tmp/pom.xml -s /usr/share/maven/ref/settings-docker.xml dependency:resolve
-CMD ["RUN YOUR CODEs"]
--- src/main/App.java
+++
@@ -1,9 +0,0 @@
-public class App
-{
- public static void main( String[] args )
- {
- DeepThought ourSuperComputer = new DeepThought();
- int ans = ourSuperComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything();
- System.out.println(ans);
- }
-}
